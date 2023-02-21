a411dd43c11f663674fc38727ac438a0e3e41e89
Guilherme Hiraoka <guilherme.hiraoka@gmail.com>
2023-02-21 14:09:54 -0300

    project ms

---
+++ mscourse/src/test/java/br/com/schinaman/mscourse/MscourseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.schinaman.mscourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
