5b4cd4e46b5b89f5ad9aaf2a0a38f820fe0de9c7
China <guilherme.hiraoka@gmail.com>
2023-02-25 13:15:28 -0300

    feat: endpoint

---
+++ mscourse/src/main/java/br/com/schinaman/mscourse/port/input/WorkerEndpoint.java
@@ -0,0 +1,27 @@
+package br.com.schinaman.mscourse.port.input;
+
+import br.com.schinaman.mscourse.infra.repositories.DTO.WorkerEntity;
+import br.com.schinaman.mscourse.infra.repositories.WorkerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping(value = "/workers")
+public class WorkerEndpoint {
+
+    @Autowired
+    private WorkerRepository workerRepository;
+
+    @GetMapping
+    public ResponseEntity<List<WorkerEntity>> findAll(){
+        List<WorkerEntity> list = workerRepository.findAll();
+        return ResponseEntity.ok(list);
+    }
+
+}
