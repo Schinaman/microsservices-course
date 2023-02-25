a603655474c951ddd2d613788c14cbaf48d0ad0a
China <guilherme.hiraoka@gmail.com>
2023-02-25 20:37:32 -0300

    feat: Workders findById

--- mscourse/src/main/java/br/com/schinaman/mscourse/port/input/WorkerEndpoint.java
+++ mscourse/src/main/java/br/com/schinaman/mscourse/port/input/WorkerEndpoint.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -22,6 +23,12 @@ public class WorkerEndpoint {
     public ResponseEntity<List<WorkerEntity>> findAll(){
         List<WorkerEntity> list = workerRepository.findAll();
         return ResponseEntity.ok(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<WorkerEntity> findById(@PathVariable Long id){
+        WorkerEntity workerObj = workerRepository.findById(id).get();
+        return ResponseEntity.ok(workerObj);
     }
 
 }
